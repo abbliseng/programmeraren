92e18bcb82fe05ec5f2e9adc5e21769e014f888e
abbliseng <54662679+abbliseng@users.noreply.github.com>
2022-06-03 22:04:46 +0200

    wow

--- programmeraren/src/App.vue
+++ programmeraren/src/App.vue
@@ -1,37 +1,26 @@
 <template>
   <div id="app">
     <b-navbar class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <!-- <b-navbar-nav>
-        <b-nav-item href="#"></b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav> -->
+      <!-- <b-navbar-nav> -->
+        <b-button href="/account" class="bg-dark" pill style="margin-left: 1rem;">
+          <b-icon icon="person-fill"></b-icon>
+        </b-button>
+      <!-- </b-navbar-nav> -->
       <a class="navbar-brand mb-0 h1" style="margin-left: 1rem;" href="/">
-        <!-- <span class="badge badge-warning">WIP</span> -->
-        <!-- <b-badge>New</b-badge> -->
-        <span class="badge bg-warning">WIP</span>
+        <!-- <span class="badge bg-warning">WIP</span> -->
         Programmeraren
       </a>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a id="projects" class="nav-item nav-link" href="/projects">Projekt</a>
-          <a id="course_material" class="nav-item nav-link disabled" href="/course_material">Läromedel</a>
-          <a id="tasks" class="nav-item nav-link disabled" href="/tasks">Uppgifter</a>
-          <a id="account" class="nav-item nav-link" href="/account">Konto</a>
-          <b-button @click="$store.dispatch('logout')" v-if="getUserStatus()">Logga ut</b-button>
-        </div>
-      </div>
+      <b-navbar-nav>
+        <a id="projects" class="nav-item nav-link" href="/projects">Projekt</a>
+        <a id="course_material" class="nav-item nav-link" href="/course_material">Läromedel</a>
+        <a id="tasks" class="nav-item nav-link" href="/tasks">Uppgifter</a>
+        <!-- <a id="account" class="nav-item nav-link" href="/account">Konto</a> -->
+      </b-navbar-nav>
     </b-navbar>
-    <b-alert 
-      :variant="$store.state.a.type"
-      :show="$store.state.a.dismissCountDown"
-      fade
-      @dismiss-count-down="countDownChanged"
-    >
+    <b-alert :variant="$store.state.a.type" :show="$store.state.a.dismissCountDown" fade
+      @dismiss-count-down="countDownChanged">
       {{ $store.state.a.message }}
     </b-alert>
-    <!-- <b-button @click="showAlert" variant="info" class="m-1">
-      Show alert with count-down timer
-    </b-button> -->
     <router-view />
   </div>
 </template>
@@ -47,22 +36,22 @@ export default {
     ...mapState(["userProfile"])
   },
   methods: {
-      getUserStatus(){
-        return this.$firebase.auth().currentUser
-      },
-      countDownChanged(dismissCountDown) {
-        this.$store.state.a.dismissCountDown = dismissCountDown
-      },
-      showAlert() {
-        // this.$store.state.a.dismissCountDown = this.$store.state.a.dismissTimer
-        this.$store.dispatch("sendAlert", {message: "hewwo", type: "warning"})
-      }
+    getUserStatus() {
+      return this.$firebase.auth().currentUser
     },
+    countDownChanged(dismissCountDown) {
+      this.$store.state.a.dismissCountDown = dismissCountDown
+    },
+    showAlert() {
+      // this.$store.state.a.dismissCountDown = this.$store.state.a.dismissTimer
+      this.$store.dispatch("sendAlert", { message: "hewwo", type: "warning" })
+    }
+  },
   mounted() {
     // console.log("Current user:",this.$firebase.auth().currentUser)
     // this.$store.commit("setCurrentUser", this.$firebase.auth().currentUser)
     // console.log(this.$store.getters.getCurrentUser)
-    if (this.$store.state.currentPage != "home" && this.$store.state.currentPage != "" && this.$store.state.currentPage != "signin") {
+    if (this.$store.state.currentPage != "home" && this.$store.state.currentPage != "" && this.$store.state.currentPage != "signin" && this.$store.state.currentPage != "account") {
       document.getElementById(this.$store.state.currentPage).className += " active"
     }
   }
--- programmeraren/src/views/AccountView.vue
+++ programmeraren/src/views/AccountView.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an account page</h1>
+  <div class="row justify-content-center no-gutters">
+    <b-card bg-variant="dark" text-variant="white" class="col-6" title="Konto" style="margin-top: 1rem;">
+      
+    </b-card>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted () {
+    // console.log("wa")
+    // this.$db.collection("users").doc(this.$firebase.auth().currentUser.uid).get()
+    //   .then((payload) => {
+    //     if ("admin" in payload.data()) {
+    //       console.log(payload.data().admin)
+    //     }
+    //     // console.log(payload.data())
+    //   })
+    console.log(this.$store.state.userProfile)
+  }
+}
+</script>
